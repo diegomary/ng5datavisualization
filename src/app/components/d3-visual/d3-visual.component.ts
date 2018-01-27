aa38275e543fc8cc3f6726f8e9c10e6e82963fd8
Diego Burlando <diego@dmm888.com>
2018-01-27 21:56:00 +0100

    Improved parametrization

--- src/app/components/d3-visual/d3-visual.component.ts
+++ src/app/components/d3-visual/d3-visual.component.ts
@@ -12,25 +12,8 @@ export class D3VisualComponent implements OnInit, AfterViewInit {
   constructor() {}
   name:string;
   vis:any={}; 
-  data:any[] = [{index:0, value: 27.4},
-                {index:1, value: 45},
-                {index:2, value: 25.50},
-                {index:3, value: 59.0},
-                {index:4, value: 66.00},
-                {index:5, value: 52.0},
-                {index:6, value: 14.20},
-                {index:7, value: 31.21},
-                {index:8, value: 34.40},
-                {index:9, value: 49.4},
-                {index:10, value: 16.28},
-                {index:11, value: 28.34},
-                {index:12, value: 51.0},
-                {index:13, value: 18.6},
-                {index:14, value: 29.6},
-                {index:15, value: 71.1},
-                {index:16, value: 36.90},
-                ];
- maxValue = Math.max.apply(Math,this.data.map((o)=>{return o.value;}));
+  data:number[] = [7.4,3.51,6.50,12.4,1.45,23.1,2.45,2.34,1.23,1.23,11,1,2,3,4,5,6];               
+  maxValue = Math.max.apply(Math,this.data.map((o)=>{return o;}));
  
 
  barColors = ():any => {
@@ -55,36 +38,36 @@ export class D3VisualComponent implements OnInit, AfterViewInit {
       .attr("width", width)
       .attr("height", height)                
       .style("padding", "30px");
+
+
     let yScale = d3.scaleLinear()
       .domain([0, this.maxValue])
-      .range([height,0]);    
+      .range([height,0])
+
     let xScale = d3.scaleLinear()
     .domain([0,data.length])
     .range([0, width]);
     let yAxis = d3.axisLeft(yScale);
-    let xAxis = d3.axisBottom(xScale);
+    let xAxis = d3.axisBottom(xScale).ticks(this.data.length);
     this.vis.append("g")          
-        .call(yAxis);
-   
+        .call(yAxis);   
     this.vis.append("g")        
         .attr("transform", "translate(0," + (height) + ")")
         .call(xAxis);
-
-
     // Bars creation
     let minigroup =  this.vis.append("g")
     .selectAll("g")
     .data(data).enter().append("g");
     minigroup.append("rect")
     .attr("width",width/data.length)
-    .attr("height", (d)=> {return (d.value *400/this.maxValue);})    
-    .attr("transform", (d)=> { return "translate(0," + (height - (d.value*400/this.maxValue)) + ")";})
-    .attr("x", (d) => {let xPos = d.index * (width/data.length); return xPos; });
-    minigroup.append("text").text((d)=> { return d.value; })
-    
-    .attr("transform", (d)=> { return "translate(0," + ((height - (d.value*400/this.maxValue))+(d.value*400/this.maxValue/2)) + ") rotate(0)";})
+    .attr("height", (d) => { return (d *400/this.maxValue);})    
+    .attr("transform", (d)=> { return "translate(0," + (height - (d*400/this.maxValue)) + ")";})
+    .attr("x", (d,i) => { let xPos = i * (width/data.length); return xPos; });
+    minigroup.append("text").text(d=>d)
+   
+    .attr("transform", (d)=> { return "translate(0," + ((height - (d*400/this.maxValue))+(d*400/this.maxValue/2)) + ") rotate(0)";})
     .attr("font-size","small")   
-    .attr("x", (d) => {let xPos = ((d.index*width/data.length)+(width/data.length/2)); return xPos;});
+    .attr("x", (d,i) => {let xPos = ((i*width/data.length)+(width/data.length/2)); return xPos;});
       //minigroup.selectAll("g:nth-child(2n) > rect").style("fill","steelblue");
       //minigroup.selectAll("g:nth-child(2n+1) > rect").style("fill","cornflowerblue");
       //minigroup.selectAll("g> text").style("fill","white");
@@ -105,8 +88,18 @@ export class D3VisualComponent implements OnInit, AfterViewInit {
      d3.select(".bar").html("");
      this.vis={};     
    }
-  ngAfterViewInit():void { 
+  ngAfterViewInit():void {
+
+
   d3.json('http://api.reddit.com/', (error, data:any) => {this.drawGraph(window.innerWidth-60,this.data);});
+
+// setInterval(()=>{
+//   this.data = Array.from({length: Math.floor(Math.random() * 40)}, () => Math.floor(Math.random() * 40));  
+//   this.maxValue = Math.max.apply(Math,this.data.map((o)=>{return o;}));
+//   this.drawGraph(window.innerWidth-60,this.data)
+// },800)
+
+
   }
 
 }
